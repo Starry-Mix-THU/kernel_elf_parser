fdcce740d718031224bdb8a77ff593b38e03cea7
mivik <mivikq@gmail.com>
2025-08-08 15:23:10 +0800

    refactor: ELFFile -> ELFHeaders, enables lazy loading

--- src/info.rs
+++ src/info.rs
@@ -1,78 +1,106 @@
 //! ELF information parsed from the ELF file
 
-use xmas_elf::program::Flags;
+use alloc::vec::Vec;
+use core::ops::Range;
+
+use xmas_elf::{
+    header::Class,
+    program::{ProgramHeader32, ProgramHeader64},
+};
 
 use crate::auxv::{AuxEntry, AuxType};
 
-/// ELF Program Header applied to the kernel
-///
-/// Details can be seen in the [ELF Program Header](https://refspecs.linuxbase.org/elf/gabi4+/ch5.pheader.html)
-pub struct ELFPH {
-    /// The start offset of the segment in the ELF file
-    pub offset: usize,
-    /// The destination virtual address of the segment in the kernel memory
-    pub vaddr: usize,
-    /// Memory size of the segment
-    pub memsz: u64,
-    /// File size of the segment
-    pub filesz: u64,
-    /// [`MappingFlags`] of the segment which is used to set the page table
-    /// entry
-    pub flags: Flags,
+pub struct ELFHeadersBuilder<'a>(ELFHeaders<'a>);
+impl<'a> ELFHeadersBuilder<'a> {
+    pub fn new(input: &'a [u8]) -> Result<Self, &'static str> {
+        Ok(Self(ELFHeaders {
+            header: xmas_elf::header::parse_header(input)?,
+            ph: Vec::new(),
+        }))
+    }
+
+    pub fn ph_range(&self) -> Range<u64> {
+        let start = self.0.header.pt2.ph_offset();
+        let size = self.0.header.pt2.ph_entry_size() as u64 * self.0.header.pt2.ph_count() as u64;
+        start..start + size
+    }
+
+    pub fn build(mut self, ph: &[u8]) -> Result<ELFHeaders<'a>, &'static str> {
+        self.0.ph = ph
+            .chunks_exact(self.0.header.pt2.ph_entry_size() as usize)
+            .map(|chunk| match self.0.header.pt1.class() {
+                Class::ThirtyTwo => {
+                    let ph: &ProgramHeader32 = zero::read(chunk);
+                    ProgramHeader64 {
+                        type_: ph.type_,
+                        offset: ph.offset as _,
+                        virtual_addr: ph.virtual_addr as _,
+                        physical_addr: ph.physical_addr as _,
+                        file_size: ph.file_size as _,
+                        mem_size: ph.mem_size as _,
+                        flags: ph.flags,
+                        align: ph.align as _,
+                    }
+                }
+                Class::SixtyFour => *zero::read(chunk),
+                Class::None | Class::Other(_) => unreachable!(),
+            })
+            .collect();
+        Ok(self.0)
+    }
+}
+
+pub struct ELFHeaders<'a> {
+    pub header: xmas_elf::header::Header<'a>,
+    pub ph: Vec<ProgramHeader64>,
 }
 
 /// A wrapper for the ELF file data with some useful methods.
 pub struct ELFParser<'a> {
-    elf: &'a xmas_elf::ElfFile<'a>,
+    headers: &'a ELFHeaders<'a>,
     /// Base address of the ELF file loaded into the memory.
     base: usize,
 }
 
 impl<'a> ELFParser<'a> {
     /// Create a new `ELFInfo` instance.
-    /// # Arguments
-    /// * `elf` - The ELF file data
-    /// * `bias` - Bias for the base address of the PIE executable.
-    pub fn new(elf: &'a xmas_elf::ElfFile, bias: usize) -> Result<Self, &'static str> {
-        if elf.header.pt1.magic.as_slice() != b"\x7fELF" {
-            return Err("invalid elf!");
-        }
-        let base = if elf.header.pt2.type_().as_type() == xmas_elf::header::Type::SharedObject {
+    pub fn new(headers: &'a ELFHeaders<'a>, bias: usize) -> Result<Self, &'static str> {
+        let base = if headers.header.pt2.type_().as_type() == xmas_elf::header::Type::SharedObject {
             bias
         } else {
             0
         };
-        Ok(Self { elf, base })
+        Ok(Self { headers, base })
     }
 
     /// The entry point of the ELF file.
     pub fn entry(&self) -> usize {
         // TODO: base_load_address_offset?
-        self.elf.header.pt2.entry_point() as usize + self.base
+        self.headers.header.pt2.entry_point() as usize + self.base
     }
 
     /// The number of program headers in the ELF file.
     pub fn phnum(&self) -> usize {
-        self.elf.header.pt2.ph_count() as usize
+        self.headers.header.pt2.ph_count() as usize
     }
 
     /// The size of the program header table entry in the ELF file.
     pub fn phent(&self) -> usize {
-        self.elf.header.pt2.ph_entry_size() as usize
+        self.headers.header.pt2.ph_entry_size() as usize
     }
 
     /// The offset of the program header table in the ELF file.
     pub fn phdr(&self) -> usize {
-        let ph_offset = self.elf.header.pt2.ph_offset() as usize;
+        let ph_offset = self.headers.header.pt2.ph_offset() as usize;
         let header = self
-            .elf
-            .program_iter()
+            .headers
+            .ph
+            .iter()
             .find(|header| {
-                (header.offset()..header.offset() + header.file_size())
-                    .contains(&(ph_offset as u64))
+                (header.offset..header.offset + header.file_size).contains(&(ph_offset as u64))
             })
             .expect("can not find program header table address in elf");
-        ph_offset - header.offset() as usize + header.virtual_addr() as usize + self.base
+        ph_offset - header.offset as usize + header.virtual_addr as usize + self.base
     }
 
     /// The base address of the ELF file loaded into the memory.
@@ -80,9 +108,8 @@ impl<'a> ELFParser<'a> {
         self.base
     }
 
-    /// The ref of the ELF file data.
-    pub fn elf(&self) -> &xmas_elf::ElfFile {
-        self.elf
+    pub fn headers(&self) -> &'a ELFHeaders<'a> {
+        self.headers
     }
 
     /// Part of auxiliary vectors from the ELF file.
@@ -108,24 +135,5 @@ impl<'a> ELFParser<'a> {
         .into_iter()
         .chain(ldso_base.into_iter().map(|base| (AuxType::BASE, base)))
         .map(|(at, val)| AuxEntry::new(at, val))
-    }
-
-    /// Read all [`self::ELFPH`] with `LOAD` type of the elf file.
-    pub fn ph_load(&self) -> impl Iterator<Item = ELFPH> + '_ {
-        // Load Elf "LOAD" segments at base_addr.
-        self.elf
-            .program_iter()
-            .filter(|ph| ph.get_type() == Ok(xmas_elf::program::Type::Load))
-            .map(|ph| {
-                let start_va = ph.virtual_addr() as usize + self.base;
-                let start_offset = ph.offset() as usize;
-                ELFPH {
-                    offset: start_offset,
-                    vaddr: start_va,
-                    memsz: ph.mem_size(),
-                    filesz: ph.file_size(),
-                    flags: ph.flags(),
-                }
-            })
     }
 }
